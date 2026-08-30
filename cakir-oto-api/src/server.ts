54e583ebd7df6d7b31bff776f4ce9231d6161dec
Ozan Çakır <ozan.cakir@vesacons.com>
2026-08-30 19:34:39 +0300

    server

--- cakir-oto-api/src/server.ts
+++ cakir-oto-api/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app.js";
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT ?? 3000);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Çakır Oto API ${port} portunda çalışıyor`);
 });
